2ba81994f9e81da6e81da57a9cb6ff0ee7233ba4
Toshiyuki NH <take90-it09-easy27@ouglook.jp>
2024-11-08 21:24:49 +0900

    fix: importing

--- directory_structure_py/__main__.py
+++ directory_structure_py/__main__.py
@@ -16,9 +16,12 @@ if __name__ == "__main__":
         "--include_root_path", dest="include_root_path", action="store_true"
     )
     parser.add_argument(
+        "--in_tree", dest="in_tree", action="store_true"
+    )
+    parser.add_argument(
         "--to_tsv", dest="to_tsv", action="store_true"
     )
     args = parser.parse_args()
     if not args.dst:
         args.dst = os.path.join(args.src, DEFAULT_OUTPUT_NAME)
-    main(args.src, args.dst, args.include_root_path, args.to_tsv)
+    main(args.src, args.dst, args.include_root_path, args.in_tree, args.to_tsv)
