0505479311b333d9c47303eb7784090b8daf2a41
Toshiyuki NH <take90-it09-easy27@ouglook.jp>
2024-11-10 16:53:46 +0900

    fix #16: add a function to generate an Id of an entity, add id to entities' metadata

--- directory_structure_py/src/main.py
+++ directory_structure_py/src/main.py
@@ -14,13 +14,39 @@ DEFAULT_OUTPUT_NAME: str = "directory_structure_metadata.json"
 JSON_OUTPUT_INDENT: int = 4
 
 
-def get_metadata_of_single_file(path: Path | str) -> Dict[str, Any]:
+def generate_id(path: Path | str, root_path: Path | str = "") -> str:
+    """Generates a unique ID from a given path, optionally relative to a root path.
+
+    The generated ID is a string representation of the path, either absolute or relative
+    to the `root_path`.  It uses a POSIX-style path representation (forward slashes).
+
+    Args:
+        path: The path to generate an ID from. Can be a `pathlib.Path` object or a string.
+        root_path: An optional root path. If provided, the generated ID will be relative
+            to this path. Can be a `pathlib.Path` object or a string.  If not provided
+            or an empty string, the absolute path is used.
+
+    Returns:
+        A string representing the unique ID of the path.
+
+    Raises:
+        ValueError: If `root_path` is provided and `path` is not a subpath of `root_path`.
+    """
+    if not root_path:
+        return str(path.absolute().as_posix())
+    return str(path.relative_to(root_path).as_posix())
+
+
+def get_metadata_of_single_file(path: Path | str, root_path: Path | str = "") -> Dict[str, Any]:
     """
     Retrieves metadata for a given file or directory using the Pathlib module.
 
     Args:
         path (Path or str): The path to the file or directory.
-        This can be a Path object or a string representing the path.
+            This can be a Path object or a string representing the path.
+        root_path (Path or str): The root path to which the path is relative.
+            This is used to set '@id.'
+            If blank, then the absolute path of 'path' is set as '@id.'
 
     Returns:
         Dict[str, Any]: A dictionary containing metadata for the file or directory.
@@ -36,6 +62,9 @@ def get_metadata_of_single_file(path: Path | str) -> Dict[str, Any]:
     if isinstance(path, str):
         path = Path(path)
     dst: Dict[str, Any] = {}
+
+    dst["@id"] = generate_id(path, root_path)
+
     if path.is_file():
         dst["type"] = "File"
     elif path.is_dir():
@@ -49,9 +78,9 @@ def get_metadata_of_single_file(path: Path | str) -> Dict[str, Any]:
         dst["name"] = os.path.splitext(path.name)[0]
         dst["extension"] = os.path.splitext(path.name)[1]
     if path.is_dir():
-        part: List[str] = [p_.name for p_ in path.iterdir()]
+        part: List[str] = [generate_id(p_, root_path) for p_ in path.iterdir()]
         if part:
-            dst["hasPart"] = part
+            dst["hasPart"] = [{"@id": p_} for p_ in part]
     dst["contentSize"] = path.stat().st_size
     dst["creationDatetime"] = datetime.datetime.fromtimestamp(
         path.stat().st_ctime
@@ -85,13 +114,13 @@ def get_metadata_of_files_in_list_format(
         containing the metadata for the files and directories within it. If `include_root_path`
         is `True`, the 'root_path' key will hold the string representation of the root directory.
     """
-    def _get_metadata_list(src: Path) -> List[Dict[str, Any]]:
+    def _get_metadata_list(src: Path, root_path: Path | str = "") -> List[Dict[str, Any]]:
         dst: List[Dict[str, Any]] = []
-        dst.append(get_metadata_of_single_file(src))
+        dst.append(get_metadata_of_single_file(src, root_path=root_path))
         if src.is_file():
             return dst
         for path_ in src.iterdir():
-            dst.extend(_get_metadata_list(path_))
+            dst.extend(_get_metadata_list(path_, root_path=root_path))
         return dst
 
     dst: Dict[str, Any] = {}
@@ -99,7 +128,7 @@ def get_metadata_of_files_in_list_format(
         src = Path(src)
     if include_root_path:
         dst["root_path"] = str(src)
-    dst["contents"] = _get_metadata_list(src)
+    dst["contents"] = _get_metadata_list(src, root_path=src)
     return dst
 
 
@@ -122,15 +151,15 @@ def json2tsv(src: str, dst: str = "") -> None:
         src (str): Path to the source JSON file.
         dst (str, optional): Path to the destination TSV file.
             Defaults to a file with the same name as `src` but with a `.tsv` extension.
-    
+
     Returns:
         None: The function writes directly to a file and does not return a value.
-    
+
     Raises:
         FileNotFoundError: If the source file does not exist.
         JSONDecodeError: If the source file is not a valid JSON.
         OSError: If there are issues reading from or writing to the file system.
-    
+
     Example:
         json2tsv("data.json", "output.tsv")
     """
@@ -171,7 +200,7 @@ def _construct_tree(
     If `tree_` is empty, the function initializes it by searching for the root node, 
     defined as the node where the "parent" key is an empty string.
     The function handles the following cases:
-    
+
     1. If a node has the type "File", it is treated as a leaf node, and recursion terminates.
     2. If a node has children (stored in the "hasPart" key), those children are recursively
     added to the tree.
@@ -180,17 +209,17 @@ def _construct_tree(
         tree_ (Dict[str, Any]): The current tree or an empty dictionary to initialize the root node.
         src (List[Dict[str, Any]]): A list of nodes, where each node is a dictionary containing
             "basename", "parent", "type", and possibly "hasPart".
-    
+
     Returns:
         Dict[str, Any]: The updated tree structure with all nested nodes attached.
 
     Example:
         tree = _construct_tree({}, nodes)
-    
+
     Notes:
         - Nodes in `src` should have "basename" and "parent" keys. 
         - Leaf nodes are those with a "type" of "File".
-    
+
     Raises:
         KeyError: If a required key is missing from any node in the source list.
     """
@@ -225,19 +254,19 @@ def list2tree(src: Dict[str, Any]) -> Dict[str, Any]:
     Args:
         src (Dict[str, Any]): A metadata dictionary with a "contents" key,
             which contains a list of node dictionaries.
-    
+
     Returns:
         Dict[str, Any]: A hierarchical tree structure where nodes are nested
             according to their parent-child relationships.
-    
+
     Example:
         tree = list2tree(metadata_dict)
-    
+
     Notes:
         - The "contents" key in `src` should be a list of dictionaries,
             each representing a node with its metadata.
         - The function relies on `_construct_tree` to recursively build the tree.
-    
+
     Raises:
         KeyError: If the "contents" key is missing in the source dictionary.
     """
@@ -259,13 +288,13 @@ def list2tree_from_file(src: Path | str) -> Dict[str, Any]:
     Args:
         src (Path | str): The path to the JSON file containing the metadata. 
                           Can be a `Path` object or a string representing the file path.
-    
+
     Returns:
         Dict[str, Any]: A hierarchical tree structure constructed from the JSON metadata.
-    
+
     Example:
         tree = list2tree_from_file("metadata.json")
-    
+
     Raises:
         FileNotFoundError: If the specified file does not exist.
         JSONDecodeError: If the file is not a valid JSON.
@@ -301,7 +330,8 @@ def main(
     Returns:
         None: The function writes the metadata to a file and does not return anything.
     """
-    data: Dict[str, Any] = get_metadata_of_files_in_list_format(src, include_root_path)
+    data: Dict[str, Any] = get_metadata_of_files_in_list_format(
+        src, include_root_path)
     with open(dst, "w", encoding="utf-8") as ff:
         json.dump(data, ff, indent=JSON_OUTPUT_INDENT)
     if to_tsv:
