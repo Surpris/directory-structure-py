9c22e794c655eea70295b23546cb51439bd05020
Toshiyuki NH <take90-it09-easy27@ouglook.jp>
2024-11-23 13:57:05 +0900

    add the "directory_only" option to the bach file

--- for_portable/directory_structure_py.bat
+++ for_portable/directory_structure_py.bat
@@ -1,4 +1,5 @@
 @echo off
-%~dp0\directory_structure_py.exe %1 --dst %~dp0\output\directory_structure_metadata.json --in_tree --to_tsv ^
+%~dp0\directory_structure_py.exe %1 --dst %~dp0\output\directory_structure_metadata.json ^
+    --in_tree --directory_only --to_tsv ^
     --log_config_path %~dp0\logging.json ^
     --log_output_path %~dp0\log\app.log
