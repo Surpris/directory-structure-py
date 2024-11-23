2d9107b1b8ed2ba2eef840440d307871eca7aa62
Toshiyuki NH <take90-it09-easy27@ouglook.jp>
2024-11-23 19:52:24 +0900

    update the batch file and pyproject.toml

--- for_portable/directory_structure_py.bat
+++ for_portable/directory_structure_py.bat
@@ -1,5 +1,7 @@
 @echo off
 %~dp0\directory_structure_py.exe %1 --dst %~dp0\output\directory_structure_metadata.json ^
-    --in_tree --structure_only --to_tsv ^
+    --in_rocrate ^
+    --to_tsv ^
+    --in_tree --structure_only ^
     --log_config_path %~dp0\logging.json ^
     --log_output_path %~dp0\log\app.log
