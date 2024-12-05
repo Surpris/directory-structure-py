d87d7dc90cc43bbf08dd7225cb420010b644648a
Toshiyuki NH <take90-it09-easy27@ouglook.jp>
2024-12-05 22:01:00 +0900

    fix #54

--- for_portable/directory_structure_py.bat
+++ for_portable/directory_structure_py.bat
@@ -1,8 +1,8 @@
 @echo off
-%~dp0\src\directory_structure_py.exe %1 --dst %~dp0output\%~n1\directory_structure_metadata.json ^
+"%~dp0\src\directory_structure_py.exe" "%~1" --dst "%~dp0output\%~n1\directory_structure_metadata.json" ^
     --in_rocrate ^
     --to_tsv ^
     --in_tree --structure_only ^
-    --log_config_path %~dp0\src\config\logging.json ^
-    --log_output_path %~dp0\log\app.log ^
-    --preview_template_path %~dp0\src\templates\preview_template.html.j2
+    --log_config_path "%~dp0\src\config\logging.json" ^
+    --log_output_path "%~dp0\log\app.log" ^
+    --preview_template_path "%~dp0\src\templates\preview_template.html.j2"
