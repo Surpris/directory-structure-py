bfc1c3c8f6bf0d5b8c4191270934c3943145bb38
Toshiyuki NH <take90-it09-easy27@ouglook.jp>
2024-12-29 20:59:44 +0900

    feature #80: add and edit scripts of the main module

---
+++ directory_structure_py/libs/directory_structure_py/directory_structure_py/__init__.py
@@ -0,0 +1,41 @@
+"""directory_structure_py module.
+"""
+
+from typing import Any
+from directory_structure_py._version import __version__
+
+
+def __getattr__(name: str) -> Any:
+    if name == "get_metadata_of_files_in_list_format":
+        from directory_structure_py_core.get_metadata import get_metadata_of_files_in_list_format
+        return get_metadata_of_files_in_list_format
+    if name == "update_statistical_info_to_metadata_list":
+        from directory_structure_py_core.get_metadata import update_statistical_info_to_metadata_list
+        return update_statistical_info_to_metadata_list
+    if name == "list2tree":
+        from directory_structure_py_core.conversion import list2tree
+        return list2tree
+    if name == "list2tree_from_file":
+        from directory_structure_py_core.conversion import list2tree_from_file
+        return list2tree_from_file
+    if name == "convert_meta_list_json_to_tsv":
+        from directory_structure_py_core.conversion import convert_meta_list_json_to_tsv
+        return convert_meta_list_json_to_tsv
+    if name == "convert_meta_list_json_to_tsv_from_file":
+        from directory_structure_py_core.conversion import convert_meta_list_json_to_tsv_from_file
+        return convert_meta_list_json_to_tsv_from_file
+    if name == "convert_meta_list_json_to_rocrate":
+        from directory_structure_py_extension.rocrate.converter import convert_meta_list_json_to_rocrate
+        return convert_meta_list_json_to_rocrate
+    raise ModuleNotFoundError(f"Could not find: {name}")
+
+
+__all__ = [
+    "get_metadata_of_files_in_list_format",
+    "update_statistical_info_to_metadata_list",
+    "list2tree",
+    "list2tree_from_file",
+    "convert_meta_list_json_to_tsv",
+    "convert_meta_list_json_to_tsv_from_file",
+    "convert_meta_list_json_to_rocrate"
+]
