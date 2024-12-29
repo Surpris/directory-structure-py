f57099c0a16ae7df0268671ae202e77aceec3ac7
Toshiyuki NH <take90-it09-easy27@ouglook.jp>
2024-12-29 17:09:09 +0900

    feature #80: fix importing

--- directory_structure_py/libs/core/directory_structure_py_core/__init__.py
+++ directory_structure_py/libs/core/directory_structure_py_core/__init__.py
@@ -1,6 +1,6 @@
 """directory_structure_py_core module.
 """
 
-from directory_structure_py.core._version import __version__
+from directory_structure_py_core._version import __version__
 
 __all__ = ['__version__']
